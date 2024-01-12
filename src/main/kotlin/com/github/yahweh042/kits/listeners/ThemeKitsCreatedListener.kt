9270777c3243b3d6e22cca9a2ff6de667bbe935d
yahweh042 <38219790+yahweh042@users.noreply.github.com>
2024-01-12 19:55:58 +0800

    ..

--- src/main/kotlin/com/github/yahweh042/kits/listeners/ThemeKitsCreatedListener.kt
+++ src/main/kotlin/com/github/yahweh042/kits/listeners/ThemeKitsCreatedListener.kt
@@ -5,6 +5,10 @@ import com.intellij.ide.AppLifecycleListener
 import com.intellij.ide.ui.LafManager
 import com.intellij.ide.ui.UITheme
 import com.intellij.openapi.application.ApplicationManager
+import org.jdesktop.swingx.plaf.UIManagerExt
+import java.util.UUID
+import java.util.function.Function
+import javax.swing.UIDefaults
 import javax.swing.UIManager
 
 internal class ThemeKitsCreatedListener : AppLifecycleListener {
@@ -16,6 +20,31 @@ internal class ThemeKitsCreatedListener : AppLifecycleListener {
             if (it.propertyName == "lookAndFeel") {
                 // applyCustomProperty()
             }
+        }
+
+        ApplicationManager.getApplication().invokeAndWait {
+
+            val themeContent = """
+                
+                {
+                    "ui": {
+                        "Button.arc": 0
+                    }
+                }
+                
+            """.trimIndent()
+
+
+            val uiTheme = UITheme.loadFromJson(
+                themeContent.encodeToByteArray(),
+                UUID.randomUUID().toString(),
+                null,
+                Function.identity()
+            )
+
+            uiTheme.applyProperties(UIManager.getDefaults())
+
+
         }
     }
 
